188fbc38929c5245bb6ea516a6d6cf9b202e661c
Adam Alsegård <adam.alsegard@gmail.com>
2015-10-20 23:20:54 +0200

    Added descriptions in portfolio

---
+++ listor/engagemang.php
@@ -0,0 +1,156 @@
+{
+"records": [
+  	{
+	  "title":"Accounter MTD2016", 
+    "description": "In charge of the finences of MTD2016. A leading position.",
+    "skills": "Leadership, big organisation, accounting, teamwork",
+  	"duration": "July 2015 - June 2016 (Ongoing)", 
+  	"volonteer": "volonteer",
+    "links": "<a href='http://www.medieteknikdagarna.se/'>MTD website</a>", 
+    "video": "",
+    "show": "true"
+	},
+
+  {
+	  "title":"Klassföreståndare", 
+    "description": "",
+  	"skills": "",
+    "duration": "",
+    "volonteer": "employment", 
+  	"links": "",
+  	"video": "",
+    "show": "true"
+	},
+
+  {
+    "title":"Klassrepresentant", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "volonteer",
+    "links": "",
+    "video": "",
+    "show": "true"
+  },
+
+  {
+    "title":"Mattementor", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "employment",
+    "links": "", 
+    "video": "",
+    "show": "true"
+  },
+
+	{
+	  "title":"Legionen 2014", 
+    "description": "",
+  	"skills": "",
+    "duration": "", 
+  	"volonteer": "volonteer",
+    "links": "", 
+  	"video": "",
+    "show": "true"
+	},
+
+  {
+    "title":"Valberedningen 2014", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "volonteer",
+    "links": "", 
+    "video": "",
+    "show": "false"
+  },
+
+  {
+    "title":"Aktivitetsledare Naffi", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "volonteer",
+    "links": "",
+    "video": "",
+    "show": "false"
+  },
+
+  {
+    "title":"Trappan", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "volonteer",
+    "links": "",
+    "video": "",
+    "show": "true"
+  },
+
+  {
+    "title":"CISV JC", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "volonteer",
+    "links": "",
+    "video": "",
+    "show": "true"
+  },
+
+  {
+    "title":"Basket", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "hobby",
+    "links": "",
+    "video": "",
+    "show": "false"
+  },
+
+  {
+    "title":"Teater", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "hobby",
+    "links": "", 
+    "video": "",
+    "show": "false"
+  },
+
+  {
+    "title":"Valberedning Småstickorna", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "volonteer",
+    "links": "", 
+    "video": "",
+    "show": "false"
+  },
+
+  {
+    "title":"Konferencier för Gala för Världens Barn", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "volonteer",
+    "links": "", 
+    "video": "",
+    "show": "false"
+  },
+
+  {
+    "title":"Konfirmationsledare", 
+    "description": "",
+    "skills": "",
+    "duration": "", 
+    "volonteer": "volonteer",
+    "links": "", 
+    "video": "",
+    "show": "false"
+  }]
+}
--- listor/portfolio.php
+++ listor/portfolio.php
@@ -1,48 +1,82 @@
 {
 "records": [
   	{
-	"title":"Trygga", 
-      	"description": "A project for the hackathon 'East Sweden Hack' for which we won a price",
-  	"duration": "Autumn 2015", 
+	  "title":"Trygga", 
+    "description": "Trygga is a service for a safer community, essentially a system for a smart panic alarm. The basic idea is to have a small device on you, as when triggered sends out a signal to a network of people in the nearby area who can interact if needed. The prototype was built with LittleBits, IFFT and meteor.js. The project was an entry at East Sweden Hack 2015 which is a hackathon at Mjärdevi Science park in Linköping. Trygga won the main price for Best Project. The price itself was a journey to Berlin in late 2015 to meet Start-ups and a couple of Internet-of-things companies (Industry 4.0). The team has continued to work on Trygga after the Hackathon (see our official website to the right for further information).",
+    "skills": "teamwork, project management, work under pressure, meteor.js, presentation",
+  	"duration": "Sept 2015 - (Ongoing)", 
   	"links": "<a href='http://eastswedenhack.se/'>East Sweden Hack - Presentation</a>", 
-  	"video": ""
+  	"video": "",
+    "show": "true"
 	},
 
-      	{
-	"title":"Reverbify", 
-      	"description": "A project in sound physics. (Course TFYA65). We did some reverb",
-  	"duration": "Sept-Okt 2015", 
+  {
+	  "title":"Reverbify", 
+    "description": "Reverbify is an application where the user can upload, record or choose a default sound that can be 'reverbified'. To 'reverbify' is to add a room's reverb to the chosen sound and then play it as it would sound in that room. This was a short group project in the course Sound Physics (TFYA65). The group of three put in a lot of more work than necessary for this type of project because we wanted to build a good application.",
+  	"skills": "meteor.js, Web Audio API, bootstrap, Ionic",
+    "duration": "Sept - Okt 2015", 
   	"links": "<a href='http://reverbify.meteor.com/'>Website</a>",
-  	"video": ""
+  	"video": "",
+    "show": "true"
 	},
+
+  {
+    "title":"adamalsegard.github.io", 
+    "description": "The very website you are looking at right now. This is my first attempt at a fully functional website, all by myself.",
+    "skills": "bootstrap, skrollr, angular.js",
+    "duration": "(Ongoing)", 
+    "links": "",
+    "video": "",
+    "show": "false"
+  },
 
 	{
-	"title":"Domescape", 
-      	"description": "A simple 3D-landscape for domes created with SGCT and cspice (which calculates the sun's correct position relative Norrköping, Sweden). The user can walk around in the landscape and speed up/down time or set a specific time to change the sun's position. Created as a group-project in the course 3D- Computer Graphics.",
-  	"duration": "Spring 2015", 
+	  "title":"Domescape", 
+    "description": "A simple 3D-landscape for cluster-based Domes created with SGCT and cspice (which in our application calculates the sun's correct position relative coordinates for Norrköping, Sweden). The user can walk around in the landscape and speed up/down time or set a specific time to change the sun's position. The landscape has some mountains and a couple of objects, all with texture. Created as a group project in the course 3D Computer Graphics (TNM061). This was the first time any of us tried SGCT. We implemented lots of stuff during the course of the project that doesn't show in our final version, mostly because we found better ways to do it all the time. There was also a couple of thing we didn't really get a hang of. Shadow mapping for example, which I worked a lot on but didn't quite get correct until deadline. You need to cmake the project before you can compile it, and it will look rather bad if you don't run it in a cluster. But you can look at https://c-student.itn.liu.se/wiki/develop:sgcttutorials:compile if you want to compile our source code.",
+  	"skills": "OpenGL, SGCT, cspice, Cmake, Linux, C#",
+    "duration": "April - May 2015", 
   	"links": "<a href='https://github.com/Danielbook/3D_Projekt'>Source code</a>", 
-  	"video": ""
+  	"video": "",
+    "show": "true"
 	},
 
-  	{
-	"title":"InterHelios", 
-      	"description": "An Android game developed during a course in user interface design and usability. It is a top-down 2D space shooter. The game is written in Java using the AndEngine framework.",
-  	"duration": "Nov-Dec 2014", 
+  {
+	  "title":"InterHelios", 
+    "description": "An Android game developed during a course in user interface design and usability (TNM040). It is a top-down 2D space shooter. The game is written in Java using the AndEngine framework. It was developed in a group of five. We didn't have specific roles or areas of responsibilities. This was my first application developed for Android, and also my first game. Works best on Nexus 5 or equally big screens.",
+  	"skills": "UX, Android Studio, Java, AndEngine",
+    "duration": "Nov-Dec 2014", 
   	"links": "<a href='https://github.com/Danielbook/TNM040-KOMA-Projekt'>Source code</a>", 
-  	"video": "<div class='embed-responsive embed-responsive-16by9'><iframe class='embed-responsive-item' src='https://www.youtube.com/embed/crZpxgsYBw8' width='420' height='315' frameborder='0' allowfullscreen></iframe></div>"},
+  	"video": "<div class='embed-responsive embed-responsive-16by9'><iframe class='embed-responsive-item' src='https://www.youtube.com/embed/crZpxgsYBw8' width='420' height='315' frameborder='0' allowfullscreen></iframe></div>",
+    "show": "true"
+  },
 
-  	{
-	"title":"Growth", 
-      	"description": "'In a future where all vegetation has died, a company succeeds in producing a synthetic substitute for food. What could have been the dawn of a new era of science turns out to only be a different sort of extinction...' <br> A sci-fi short film done with a couple of classmates in the course Digital Media. Big thanks to all actors who contributed without payment.",
-  	"duration": "Autumn 2013", 
+  {
+	  "title":"Growth", 
+    "description": "'In a future where all vegetation has died, a company succeeds in producing a synthetic substitute for food. What could have been the dawn of a new era of science turns out to only be a different sort of extinction...' A sci-fi short film done with a couple of classmates in the course Digital Media (TNM088). Big thanks to all actors and extras who contributed without payment. The film was produced during a couple of weeks in the autumn of 2013. I was the director, editor, writer, casting- and ADR-director. I also did a promotional website prior to our deadline. After our first showing my cinematographer Benjamin Wiberg did a new website. both can be seen to the right.",
+  	"skills": "filmmaking, leadership, basic web development",
+    "duration": "Okt - Dec 2013", 
   	"links": "<a href='growth/old/index.html'>Website 1</a><br><a href='growth/index.html'>Website 2 (by Benjamin Wiberg)</a>", 
-  	"video": "<div class='embed-responsive embed-responsive-16by9'><iframe width='420' height='315' class='embed-responsive-item' src='https://player.vimeo.com/video/85702031'  frameborder='0' allowfullscreen></iframe></div>"}, 
+  	"video": "<div class='embed-responsive embed-responsive-16by9'><iframe width='420' height='315' class='embed-responsive-item' src='https://player.vimeo.com/video/85702031'  frameborder='0' allowfullscreen></iframe></div>",
+    "show": "true"
+  }, 
   	  
-  	{
-	"title":"Legomania", 
-        "description": "A website about Lego! A group-project in electronic publishing where we search for specifik lego bits in a database.",
-  	"duration": "Autumn 2013", 
+  {
+	  "title":"Legomania", 
+    "description": "A website about Lego! A group project in Electronic Publishing (TNMK30) where the user can search for specific Lego bits in a database. This was my (and the rest of the group's) first website and it was developed in a group with five members. I was in charge of the database-handling together with another group member where the other was in charge of the design and forum.",
+  	"skills": "php, MySQL, basic web development",
+    "duration": "Nov - Dec 2013", 
   	"links": "<a href='http://www.student.itn.liu.se/~adaal265/TNMK30/Projekt/Legomania-master/Legomania-master/main.php'>Check it out!</a>", 
-  	"video": ""
-	}]
+  	"video": "",
+    "show": "true"
+	}, 
+      
+  {
+    "title":"Earlier short films", 
+    "description": "I studied at a film school i Stockholm for a year and did some short films. A couple of them can be seen in my different channels. I have also done some films in later years. Be sure to check out 'appearances' as well.",
+    "skills": "filmmaking, team work",
+    "duration": "Aug 2012 - May 2013", 
+    "links": "<a href='https://vimeo.com/adamalsegard'>Check out my vimeo channel!</a><br><a href='https://www.youtube.com/user/questisiones/videos'>And my youtube channel</a>", 
+    "video": "",
+    "show": "false"
+  }]
 }
