527dee615da654f954ccf3946b1e230dcc39f7fe
Adam Alsegård <adam.alsegard@gmail.com>
2015-10-29 23:15:14 +0100

    Added some text in commitments

--- listor/engagemang.php
+++ listor/engagemang.php
@@ -1,21 +1,21 @@
 {
 "records": [
   	{
-	  "title":"Accounter MTD2016", 
-    "description": "In charge of the finances of MTD2016.",
-    "skills": "Leadership, big organisation, accounting, teamwork",
+	  "title":"Paymaster MTD2016", 
+    "description": "Responsible for budgeting and accounting the finances of MTD2016. MTD (Medieteknikdagarna) is a committee of the Media Technology Association (Medietekniksektionen) with the objective to arrange a Career Fair for the students at MT. This implied to find different companies and lecturers that will interest and inspire the students of MT and, hopefully, help them to bind some valuable connections with the companies. Maybe somebody can find where to write their master thesis for example. I was also responsible, together with the project manager, for the recruitment for the remaining positions at the committee. MTD is one of the biggest commitments in the MT-association and it's a huge responsibility, especially as paymaster as I am one of two authorized signatories and in charge of the 370 000 SEK budget.",
+    "skills": "leadership, adminitration of a big organisation, accounting, teamwork",
   	"duration": "July 2015 - (Ongoing)", 
   	"volonteer": "volonteer",
-    "links": "<a href='http://www.medieteknikdagarna.se/'>MTD website</a>", 
+    "links": "<a href='http://www.medieteknikdagarna.se/'>MTD16's website</a>", 
     "video": "",
     "show": "true"
 	},
 
   {
-	  "title":"Klassföreståndare", 
-    "description": "",
-  	"skills": "Leadership",
-    "duration": "July 2015 - okt 2015",
+	  "title":"Form teacher (Klassföreståndare)", 
+    "description": "As a form teacher I was employed by Linköping University to guide the new MT-students during the first few weeks of their first semester. In total I held 6 lectures where I taught everything from how to use the 'student portal' to different study techniques. During the welcoming period I also participated in most activities as to gain my students' trust, which I feel I did. It was lots of fun.",
+  	"skills": "leadership, teaching, responsible, organized",
+    "duration": "July 2015 - Okt 2015",
     "volonteer": "employment", 
   	"links": "",
   	"video": "",
@@ -23,31 +23,42 @@
 	},
 
   {
-    "title":"Aktivitetsledare Naffi", 
-    "description": "",
-    "skills": "",
+    "title":"Activity Leader Naffi", 
+    "description": "NAFFI (Norrköpings Akademiska Förening För Idrott) is the sports organisation for students in Norrköping. I was nominated for activity leader for the basketball practice. During this time I held the practice once a week. I used to prepare some fundamental drill each time because most of the participants hadn't played at all before.",
+    "skills": "leadership, teaching, basketball",
     "duration": "Sept 2015 - (Ongoing)", 
     "volonteer": "volonteer",
-    "links": "",
+    "links": "<a href="http://naffi.studorg.liu.se/">NAFFI's homepage</a>",
     "video": "",
     "show": "false"
   },
 
   {
-    "title":"Klassrepresentant", 
-    "description": "",
-    "skills": "",
+    "title":"Class representative", 
+    "description": "Class representative for LinTek’s Educational Board. We are the link between the examiner and the students. We evaluate all courses afterwards to improve the program. We also held study-rooms and some other activities.",
+    "skills": "communication, organization",
     "duration": "Sept 2013 - (Ongoing)", 
     "volonteer": "volonteer",
-    "links": "",
+    "links": "<a href="http://www.lintek.liu.se/utbildning/foerbaettra-din-utbildning/">LinTek's desciption of the position</a>",
     "video": "",
     "show": "true"
   },
 
   {
-    "title":"Mattementor", 
-    "description": "",
-    "skills": "",
+    "title":"Stubinen", 
+    "description": "Stubinen is the Student Film Union in Norrköping. I was a board member for a year. I was the event planner. Stubinen arrange a film showing at the local cinema Cnema every other week. We also held a couple of smaller events during the year.",
+    "skills": "event planning",
+    "duration": "May 2014 - May 2015", 
+    "volonteer": "volonteer",
+    "links": "<a href="http://stubinen.org/">Stubinen's official website</a>", 
+    "video": "",
+    "show": "false"
+  },
+
+  {
+    "title":"Mathematics tutor", 
+    "description": "Mathematics tutor for the new students in the first 3 courses in mathematics (Foundation in Math, Linear Algebra and Calculus I). As a tutor I held a math class about once a week and answered the students questions and demonstrated solutions on the white board. ",
+    "skills": "teaching, explaining",
     "duration": "Aug 2014 - Mars 2015", 
     "volonteer": "employment",
     "links": "", 
@@ -57,53 +68,53 @@
 
 	{
 	  "title":"Legionen 2014", 
-    "description": "",
-  	"skills": "",
+    "description": "Legionen is part of the Welcoming Committee for LinTek. LinTek is the Student Union for the Institute of Technology at Linköping University. In Legionen I was responsible for the activities during the welcoming weeks for the new students in August 2014. Part of being a 'phadderist' is to grow your beard and dress up in costume. We role play during the first few weeks while we lead the activities and perform at the parties.",
+  	"skills": "cooperation, planning, leadership, organization, creativity, improvisation",
     "duration": "Jan 2014 - Jan 2015", 
   	"volonteer": "volonteer",
-    "links": "", 
+    "links": "<a href="https://www.youtube.com/playlist?list=PLu8Zcm3s2U4sMBHBFAzeGou1w8_fchmo9">Some of our performances</a><br><a href="2014.legionen.nu">Our old website</a>", 
   	"video": "",
     "show": "true"
 	},
 
   {
-    "title":"Valberedningen 2014", 
-    "description": "",
-    "skills": "",
+    "title":"Caucus for the MediaTechnology Association", 
+    "description": "I was part of a group that interviewed and nominated students for positions as next year's Board Members.",
+    "skills": "interviewing, compromising",
     "duration": "Nov 2013 - Mars 2014", 
     "volonteer": "volonteer",
-    "links": "", 
+    "links": "<a href="http://medieteknik.nu/se/association">Current Board Members</a>", 
     "video": "",
     "show": "false"
   },
 
   {
     "title":"Trappan", 
-    "description": "",
-    "skills": "",
-    "duration": "Sept 2013 - May 2015", 
-    "volonteer": "volonteer",
-    "links": "",
+    "description": "Trappan is the Student Union House in Norrköping. I've worked there on and off during my first three years at Linköping University. I've worked as event planner, bouncer, bartender and sound technician. I worked there about three times a month generally.",
+    "skills": "public relations, serviceminded, social interaction",
+    "duration": "Sept 2013 - (Ongiong)", 
+    "volonteer": "volonteer/employed",
+    "links": "<a href="http://trappan.nu/">Trappan's official website</a>",
     "video": "",
     "show": "true"
   },
 
   {
     "title":"CISV JC", 
-    "description": "",
-    "skills": "",
+    "description": "CISV (Children's International Summer Villages) is an organisation with a peaceful world as a vision. To reach this goal they arrange a bunch of different gatherings. One, the original, is a summer village for eleven year old children from twelve different countries who come and live and play together for a month. Each country send a delegation of two girls and two boys and a leader. Then there are six junior counselors (JC's) as well. As JC you participate in the planning och leading of activities together with the rest of the leaders. I was also in charge of the Swedish delegation one weekend when the original leader was away. ",
+    "skills": "leadership, openness, social relations, mindfulness",
     "duration": "Summer 2008", 
     "volonteer": "volonteer",
-    "links": "",
+    "links": "<a href="http://www.cisv.org/">CISV official website</a>",
     "video": "",
     "show": "true"
   },
 
   {
-    "title":"Basket", 
-    "description": "",
-    "skills": "",
-    "duration": "Långt", 
+    "title":"Basketball", 
+    "description": "I played a lot of sports when I was younger. I tried most of the common team-sports in Sweden and some individual as well. Basketball was the one I fell for and I played for lots of years. I made it to some regional teams and even to the youth national team when I was fifteen years old (U15 landslaget) . My coaches told me I probably could have made it to Euroleague, but for a couple of reasons I didn't want to commit and venture for a professional career. However I kept playing on my own accord and I still play a little in my spare time.",
+    "skills": "leadership, stammina, teambuilding",
+    "duration": "Since I was 13 and still ongoing, more or less", 
     "volonteer": "hobby",
     "links": "",
     "video": "",
@@ -111,10 +122,10 @@
   },
 
   {
-    "title":"Teater", 
-    "description": "",
-    "skills": "",
-    "duration": "Ett tag", 
+    "title":"Theater", 
+    "description": "I acted in my teens. During a period of about 6 years I was in over a dozen plays. I've acted in some of the most famous Swedish plays, as well as some musicals and one or two Christmas shows. It was probably this that lead me to film. I've also acted in a couple of short films, commercials and music videos while I lived in Stockholm.",
+    "skills": "acting, rhetoric, dramarturgy, empathy, expression",
+    "duration": "From when I was about 15 and still ongoing, more or less", 
     "volonteer": "hobby",
     "links": "", 
     "video": "",
@@ -122,10 +133,10 @@
   },
 
   {
-    "title":"Valberedning Småstickorna", 
-    "description": "",
-    "skills": "",
-    "duration": "Typ 2 år?", 
+    "title":"Caucus for the theater union Småstickorna", 
+    "description": "I was in a group of three who tried to find new candidates for the Board of Småstickorna.",
+    "skills": "approaching new people",
+    "duration": "Nov 2008 - Nov 2010", 
     "volonteer": "volonteer",
     "links": "", 
     "video": "",
@@ -133,21 +144,21 @@
   },
 
   {
-    "title":"Konferencier för Gala för Världens Barn", 
-    "description": "",
-    "skills": "",
-    "duration": "För kort för att nämnas...", 
+    "title":"Master of Ceremonies for Världens Barn, 
+    "description": "Together with five others I acted as master of ceremonies on a gala for the world's children (Världens Barn)",
+    "skills": "rhetoric, presentation",
+    "duration": "Nov 2009", 
     "volonteer": "volonteer",
-    "links": "", 
+    "links": "<a href="https://start.varldensbarn.se/">Världens barn official website</a>", 
     "video": "",
     "show": "false"
   },
 
   {
-    "title":"Konfirmationsledare", 
-    "description": "",
-    "skills": "",
-    "duration": "Ett par år", 
+    "title":"Confirmation leader", 
+    "description": "I was a junior leader for a confirmation group at the Swedish Church in Jönköping (Svenska Kyrkan Jönköping, Sofia församling). Each year we went on a 10-day canoe-trip in the wilderness of northern Sweden. No electronics were allowed and we brought all the food and equipment we needed. We were usually about 100 people, all in all. There was also a week-long (civilized) camp in the end of the summer.",
+    "skills": "leadership, survival skills, canoeing",
+    "duration": "Sept 2007 - Sept 2010", 
     "volonteer": "volonteer",
     "links": "", 
     "video": "",
